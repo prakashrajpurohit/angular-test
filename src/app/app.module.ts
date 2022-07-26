210211f8ff074149c7786102e59a5c2958ad4edd
Prakash Rajpurohit <prakash@hazedawn.com>
2022-07-26 21:27:28 +0530

    update according tarun sir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    CalendarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  show:boolean=true
+  showon:boolean=false
+  shows:boolean=true
+  showons:boolean=false
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  toogle(){
+    this.show=!this.show
+    this.showon=!this.showon
+  }
+  toogles(){
+    this.shows=!this.shows
+    this.showons=!this.showons
+  }
 }
